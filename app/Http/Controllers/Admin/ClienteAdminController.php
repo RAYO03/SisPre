2033f12c33eb0f21658f3964054bc814264a6295
MontanoOquitaGabrielUTH <a24311016@uthermosillo.edu.mx>
2026-05-29 15:50:34 -0700

    refactor: usar modelo User en ClienteAdminController para obtener sus datos

--- app/Http/Controllers/Admin/ClienteAdminController.php
+++ app/Http/Controllers/Admin/ClienteAdminController.php
@@ -3,25 +3,34 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Cliente;
+use App\Models\User;
 
 class ClienteAdminController extends Controller
 {
     public function index()
     {
-        $clientes = Cliente::with('user')->latest()->get();
+        $clientes = User::with('cliente')
+            ->where('tipo_usuario', 'cliente')
+            ->latest()
+            ->get();
 
         return view('admin.clientes', compact('clientes'));
     }
 
     public function show($id)
     {
-        $cliente = Cliente::with([
-            'user.solicitudes',
-            'user.prestamos',
-            'user.pagos'
-        ])->findOrFail($id);
+        $user = User::with([
+            'cliente',
+            'solicitudes',
+            'prestamos',
+            'pagos'
+        ])
+            ->where('tipo_usuario', 'cliente')
+            ->findOrFail($id);
 
-        return view('admin.cliente-detalle', compact('cliente'));
+        return view('admin.cliente-detalle', [
+            'user' => $user,
+            'cliente' => $user->cliente,
+        ]);
     }
 }
